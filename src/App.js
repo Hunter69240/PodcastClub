cb63162d422c79a73059ae811f904788e2f22f15
Hunter69240 <aadishds@gmail.com>
2025-05-08 13:13:59 +0530

    Home Finally done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router';  // Import Routes properly
 import Home from '../src/Screens/Home'; 
-import AboutUs from '../src/Screens/AboutUs'; // Make sure the path is correct
+
 
 function App() {
   return (
     <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/AboutUs" element={<AboutUs />} />
         </Routes>
-      
     </Router>
   );
 }
